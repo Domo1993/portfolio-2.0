e47dab38b3470213e32674b9233ee630885cb460
dominic <dominic@bnry.digital>
2020-09-09 18:32:01 +0200

    another commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const myObject = {
+	title: "Pokemon Stuff",
+	list: [
+		{ name: "pikachu", type: "electric" },
+		{ name: "squirtle", type: "water" },
+		{ name: "bulbasaur", type: "grass" },
+	],
+};
+
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+	res.statusCode = 200;
+	res.json({ myObject });
+};
